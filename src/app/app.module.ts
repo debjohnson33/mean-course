af43c04f0bec9769cf43bd29f1cfc69334fd9d7f
debjohnson <debjohnson_11@yahoo.com>
2018-06-13 19:28:29 -0500

    Added angular material to app/post-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent} from './posts/post-create/post-create.component';
@@ -12,7 +15,10 @@ import { PostCreateComponent} from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
