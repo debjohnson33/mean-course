e688218f3c02ee611b560639989e456db63a4d6b
debjohnson <debjohnson_11@yahoo.com>
2018-06-26 15:44:54 -0500

    Moved posts routes to separate file

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -36,9 +36,7 @@ export class PostsService {
 	}
 
 	getPost(id: string) {
-		return this.http.get<{ _id: string; title: string; content: string >(
-			"http://localhost:3000/api/posts/" + id
-		);
+		return this.http.get<{ _id: string; title: string; content: string>("http://localhost:3000/api/posts/" + id);
 	}
 
 	addPost(title: string, content: string) {
