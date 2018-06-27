bb0d6df61992de2fe8f3f0c47bbf074536ef4d7d
debjohnson <debjohnson_11@yahoo.com>
2018-06-26 21:12:37 -0500

    Added front-end uploading/edited backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -22,12 +22,12 @@ const storage = multer.diskStorage({
 	},
 	filename: (req, file, cb) => {
 		const name = file.originalname.toLowerCase().split(' ').join('-');
-		const ext = MIME_TYPE_MAP(file.mimetype);
+		const ext = MIME_TYPE_MAP[file.mimetype];
 		cb(null, name + '-' + Date.now() + '.' + ext);
 	}
 });
 
-router.post('', multer(storage).single("image"), (req, res, next) => {
+router.post('', multer({storage: storage}).single("image"), (req, res, next) => {
 	const post = new Post({
 		title: req.body.title,
 		content: req.body.content
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -71,15 +71,23 @@ export class PostCreateComponent implements OnInit {
 		reader.readAsDataURL(file);
 	}
 
-	onSavePost(form: NgForm) {
+	onSavePost() {
 		if (this.form.invalid) {
 			return;
 		}
 		this.isLoading = true;
 		if (this.mode === 'create') {
-			this.postsService.addPost(this.form.value.title, this.form.value.content);
+			this.postsService.addPost(
+				this.form.value.title, 
+				this.form.value.content,
+				this.form.value.image
+			);
 		} else {
-			this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+			this.postsService.updatePost(
+				this.postId, 
+				this.form.value.title, 
+				this.form.value.content
+			);
 		}
 		this.form.reset();
 	}
