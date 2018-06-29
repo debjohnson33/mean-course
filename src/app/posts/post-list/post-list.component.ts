dc123e253bffc62df2aee107aee14355806a5573
debjohnson <debjohnson_11@yahoo.com>
2018-06-29 12:31:19 -0500

    Edited UI posts to reflect authentication status

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
 	selector: 'app-post-list',
@@ -24,9 +25,11 @@ export class PostListComponent implements OnInit, OnDestroy {
 	postsPerPage = 2;
 	currentPage = 1;
 	pageSizeOptions = [1, 2, 5, 10];
+	userIsAuthenticated = false;
 	private postsSub: Subscription;
+	private authStatusSub: Subscription;
 
-	constructor(public postsService: PostsService) {}
+	constructor(public postsService: PostsService, private authService: AuthService) {}
 
 	ngOnInit() {
 		this.postsService.getPosts(this.postsPerPage, this.currentPage);
@@ -37,6 +40,13 @@ export class PostListComponent implements OnInit, OnDestroy {
 				this.totalPosts = postData.postCount;
 				this.posts = postData.posts;
 			});
+		this.userIsAuthenticated = this.authService.getIsAuth();
+		this.authStatusSub = this.authService
+			.getAuthStatusListener()
+			.subscribe(isAuthenticated => {
+				this.userIsAuthenticated = isAuthenticated;
+			});
+
 	}
 
 	onChangedPage(pageData: PageEvent) {
@@ -55,5 +65,6 @@ export class PostListComponent implements OnInit, OnDestroy {
 
 	ngOnDestroy() {
 		this.postsSub.unsubscribe();
+		this.authStatusSub.unsubscribe();
 	}
 }
