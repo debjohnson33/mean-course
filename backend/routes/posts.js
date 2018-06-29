1e32d343746033cb8cfae655cbdc23c9879a2be7
debjohnson <debjohnson_11@yahoo.com>
2018-06-29 16:50:10 -0500

    Added authorization to edit/delete for the post user only

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -59,10 +59,12 @@ router.put("/:id", checkAuth, multer({storage: storage}).single("image"), (req, 
 		content: req.body.content,
 		imagePath: imagePath
 	});
-	console.log(post);
-	Post.updateOne({_id: req.params.id}, post).then(result => {
-		console.log(result);
-		res.status(200).json({message: "Update succesful!"}); 
+	Post.updateOne({_id: req.params.id, creator: req.userData.userId }, post).then(result => {
+		if (result.nModified > 0) {
+			res.status(200).json({message: "Update succesful!"});	
+		} else {
+			res.status(401).json({message: "Not authorized!"});
+		}
 	});
 });
 
@@ -101,9 +103,13 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.delete("/:id", checkAuth, (req, res, next) => {
-	Post.deleteOne({_id: req.params.id}).then(result => {
-		console.log(result);
-		res.status(200).json({message: 'Post deleted'});
+	Post.deleteOne({ _id: req.params.id, creator: req.userData.userId}).then(
+		result => {
+			if (result.n > 0) {
+				res.status(200).json({message: "Deletion succesful!"});	
+			} else {
+				res.status(401).json({message: "Not authorized!"});
+			}
 	});
 });
 
